7f3d9e2e23138c547db76e9756a7d66e75adb6bb
edme88 <edmealiciardi@gmail.com>
2026-09-21 11:22:11 -0300

    feat: renderizar catalogo

--- 3-Js/tienda.js
+++ 3-Js/tienda.js
@@ -77,3 +77,21 @@ mostrarModal = () => {
 cerrarModal = () => {
   document.getElementById("modal").style.display = 'none';
 }
+
+/**
+ * Mostrar el catalogo de productos en la seccion main
+ * @method mostrarCatalogo
+ */
+mostrarCatalogo = () => {
+  let contenido = "";
+
+  productos.forEach((producto) => {
+    contenido += `<div>
+                    <img src="https://ucc-tallerdesarrolloweb.github.io/filminas/images/ejercicios/${producto.imagen}" alt="${producto.nombre}">
+                    <h3>${producto.nombre}</h3>
+                    <button type="button" onclick="mostrarModal()">Ver detalle de Producto</button>
+                 </div>`;
+  });
+
+  document.getElementById("catalogo").innerHTML = contenido;
+}
