b85c66bd112ac69c1bc12259160bb4983aa02595
edme88 <edmealiciardi@gmail.com>
2026-09-21 08:43:19 -0300

    feat: funcion grados-radianes

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -29,3 +29,22 @@ function convertirUnidades(id, valor){
         document.getElementById("pie").value = valor*0.0833333;
     }
 }
+
+/**
+ * Conversion de grados a radianes
+ * @method convertirGR
+ * @param {string} id - Id del elemento input en el html
+ */
+function convertirGR(id){
+    let grad, rad;
+
+    if(id==="grados"){
+        grad = document.getElementById("grados").value;
+        rad = grad*Math.PI/180;
+    }else{
+        rad = document.getElementById("radianes").value;
+        grad = rad*180/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
