cb3c09f899c0204fe7de278ab6144c68e265c068
edme88 <edmealiciardi@gmail.com>
2026-09-21 09:23:55 -0300

    feat: ejercicio mostrar-ocultar

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -59,3 +59,19 @@ function convertirGR(id){
     document.getElementById("grados").value = grad;
     document.getElementById("radianes").value = rad;
 }
+
+/**
+ * Mostrar u ocultar div segun seleccion del usuario
+ * @method mostrarOcultar
+ * @param {string} valor - Valor del id del radio button seleccionado
+ */
+mostrarOcultar = (valor) => {
+    const displayDiv = valor==="val_mostrar" ? 'block' : 'none';
+    document.getElementById("unDiv").style.display = displayDiv;
+
+    // if(valor==="val_mostrar"){
+    //     document.getElementById("unDiv").style.display = 'block';
+    // }else{
+    //     document.getElementById("unDiv").style.display = 'none';
+    // }
+}
